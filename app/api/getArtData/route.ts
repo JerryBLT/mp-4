0bf1c20c979994d3e48d7f263cbe038b7177b2b5
Jerry Teixeira <jerrybt@bu.edu>
2025-04-05 21:19:46 -0400

    fixed error

--- app/api/getArtData/route.ts
+++ app/api/getArtData/route.ts
@@ -5,7 +5,7 @@ export async function GET() {
     try {
         const artData = await getArtData();
         return NextResponse.json(artData);
-    } catch (e) {
+    } catch {
         return NextResponse.json(
             { e: 'Failed to fetch art data. Please try again later.' }, { status: 500 }
         );
